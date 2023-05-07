0e5c33eee7e6e766a6a7a17dfa50b664f5ade87f
fjarm <129107554+fjarm@users.noreply.github.com>
2023-05-07 16:58:36 -0400

    Fix squares not generating correctly and code grid
    Moved square generation into the for loop because only one square was
    being generated.

--- scripts.js
+++ scripts.js
@@ -2,12 +2,10 @@ function generateSquares()
 {
     const grid = document.querySelector(".grid");
 
-    let newSquare = document.createElement("div");
-    newSquare.setAttribute('style', 'height: 100px; width: 100px; background: #4C3D3D;');
-    newSquare.classList.add('square');
-
     for (i = 0; i < 16; i++)
     {
+        let newSquare = document.createElement("div");
+        newSquare.classList.add('square');
         grid.appendChild(newSquare);
     }
 }
