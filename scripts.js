3b60b85b7f5d710b4eed66290256c8eb53099e97
fjarm <129107554+fjarm@users.noreply.github.com>
2023-05-09 13:52:52 -0400

    Generate squares based on user input

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-function generateSquares()
+function generateSquares(squareAmount)
 {
     const grid = document.querySelector(".grid");
 
-    for (i = 0; i < 16; i++)
+    for (i = 0; i < squareAmount; i++)
     {
         let newSquare = document.createElement("div");
         newSquare.classList.add('square');
@@ -21,4 +21,9 @@ function squareHover(square)
 function startButton()
 {
     let squareAmount = prompt("How many squares would you like in your canvas? (Max 100)")
+    while (squareAmount > 100 || squareAmount < 1)
+    {
+        squareAmount = prompt("Sorry, please choose a number between 1 and 100.")
+    }
+    generateSquares(squareAmount)
 }
